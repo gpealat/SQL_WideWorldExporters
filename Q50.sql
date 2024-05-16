c88dcbca813031d4876dd9cd6c254a07ea2cd6cd
gpealat <167517140+gpealat@users.noreply.github.com>
2024-05-16 15:15:06 +0200

    Create Q50.sql

---
+++ Q50.sql
@@ -0,0 +1,37 @@
+-- Q50: Find the person (people) name, quantity of items ordered from people, orders and order lines.
+USE WideWorldImporters
+
+-- Relational Algebra
+-- Get all customers who are not companies
+-- T1 = σ_Sales.Customer(BuyingGroupID IS NULL)
+-- T2 = π_T1( CustomerID )
+
+-- Get all OrderID for the customers
+-- T3 = σ_Sales.Orders( CustomerID = T2.CustomerID )
+-- T4 = π_T3( OrderID )
+
+-- Get all StockItemsID for orderID
+-- T3 = σ_Sales.OrderLines( OrderID = T4.OrderID )
+-- T4 = π_T3( Quantity )
+
+SELECT
+	SUM(orderline.Quantity) AS TotalQuantityBought, T4.CustomerName
+FROM
+	Sales.OrderLines as orderline,
+	(
+	SELECT
+		[order].OrderID, T2.CustomerID, T2.CustomerName
+	FROM
+		Sales.Orders as [order],
+		(
+		SELECT CustomerID, CustomerName
+		FROM Sales.Customers 
+		WHERE BuyingGroupID IS NULL
+		) AS T2
+	WHERE
+		[order].CustomerID = T2.CustomerID
+	) AS T4
+WHERE
+	orderline.OrderID = T4.OrderID
+GROUP BY
+	T4.CustomerName
